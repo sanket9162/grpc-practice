8a9c729af93d4c6f76e1d391c5c866e0d6169889
sanket9162 <sanketgondhali55@gmail.com>
2025-10-24 10:51:35 +0530

    connect client to server

---
+++ grpc-client/client.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln("Did not connect:", err)
+	}
+	defer conn.Close()
+
+}
