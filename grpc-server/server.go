0cd348a7ea0a392d40954ac0074bad48c687e663
sanket9162 <sanketgondhali55@gmail.com>
2025-10-24 10:25:01 +0530

    implement Add func to server

--- grpc-server/server.go
+++ grpc-server/server.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	pb "github.com/sanket9162/go-grpc/proto/gen"
 	"google.golang.org/grpc"
 )
+
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error) {
+	return &pb.AddResponse{
+		Sum: req.A + req.B,
+	}, nil
+}
 
 func main() {
 	port := ":50051"
@@ -14,8 +26,11 @@ func main() {
 		log.Fatal("Failed to listen:", err)
 	}
 
-	log.Println("server is running on port", port)
 	grpcserver := grpc.NewServer()
+
+	pb.RegisterCalculatorServer(grpcserver, &server{})
+
+	log.Println("server is running on port", port)
 	err = grpcserver.Serve(lis)
 	if err != nil {
 		log.Fatal("Failed to serve:", err)
